b63cb4cabb440ca697f35aea6841c073c6af8f93
ThomasMin146 <bencros444@azet.sk>
2023-06-16 13:51:29 +0200

    home page visual update

--- app/src/main/java/com/thomas/myprogress/HomePage.java
+++ app/src/main/java/com/thomas/myprogress/HomePage.java
@@ -1,32 +1,35 @@
 package com.thomas.myprogress;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class HomePage extends AppCompatActivity {
+    CardView startWorkoutCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
 
-        Button startButton = findViewById(R.id.startButton);
+        startWorkoutCard = findViewById(R.id.startWorkoutCard);
 
-        Button lastButton = findViewById(R.id.lastButton);
-        Button allButton = findViewById(R.id.allButton);
-
-        startButton.setOnClickListener(new View.OnClickListener() {
+        startWorkoutCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HomePage.this, StartWorkoutPage.class);
                 startActivity(intent);
+
             }
         });
+
+
 
     }
 }
