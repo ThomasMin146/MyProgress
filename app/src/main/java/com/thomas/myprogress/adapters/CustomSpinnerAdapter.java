a3d5874e92ac2c0cfbdb1d2af0da1ee9c08b1cb3
ThomasMin146 <bencros444@azet.sk>
2023-08-24 11:47:25 +0200

    UI changes

--- app/src/main/java/com/thomas/myprogress/adapters/CustomSpinnerAdapter.java
+++ app/src/main/java/com/thomas/myprogress/adapters/CustomSpinnerAdapter.java
@@ -2,6 +2,7 @@ package com.thomas.myprogress.adapters;
 
 import android.content.Context;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
